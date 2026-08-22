1e891dd52e22e63a36103e89c36049cb2c93d6d5
DUT1K1 <david.maisuradze0920@gmail.com>
2026-08-22 20:50:24 +0400

    add robots txt and sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import remarkCodeFenceMath from "./src/plugins/remark-code-fence-math.mjs";
 
 export default defineConfig({
-  site: "https://davitmaisuradze.com",
-  integrations: [react()],
+  site: "https://www.davitmaisuradze.com",
+  integrations: [react(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath, remarkCodeFenceMath],
     rehypePlugins: [rehypeKatex],
